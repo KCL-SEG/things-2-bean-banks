95911e983cd154e78493729568dc6e5b8ce21869
Ben Banks <benjamin.banks960@gmail.com>
2022-10-15 17:54:02 +0100

    Add things form with constraints

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,14 @@
 """Forms of the project."""
+from django import forms
+#from django.core.validators import MinValueValidator, MaxValueValidator
+from .models import Thing
 
 # Create your forms here.
+class ThingsForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {'description': forms.Textarea()}
+    #name = forms.charField(label="Name", max_length=35, unique=True))
+    #description = forms.charField(label="Description", max_length=120, blank=True)
+    #quantity = forms.charField(label="NumberInput", validators=[MinValueValidator(0),MaxValueValidator(50))
--- things/models.py
+++ things/models.py
@@ -2,9 +2,9 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 class Thing(models.Model):
-    name = models.CharField(max_length=35, unique=True)
-    description = models.CharField(max_length=120, blank=True)
-    quantity = models.IntegerField(
+    name = models.CharField(verbose_name='Name', max_length=35, unique=True)
+    description = models.CharField(verbose_name='Description', max_length=120, blank=True)
+    quantity = models.IntegerField(verbose_name='Quantity',
         validators=[MinValueValidator(0),MaxValueValidator(50)]
     )
     created_at = models.DateTimeField(auto_now_add=True)
